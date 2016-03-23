d5ba6789d8e8ae0af2741c413108f8dba98bf54f
Sauli Ruuska <sauli.ruuska@jalonne.fi>
2016-03-23 13:01:56 +0200

    Add automatic creation of database

--- parekori.py
+++ parekori.py
@@ -1,5 +1,7 @@
 import sqlite3
 import flask
+from contextlib import closing
+
 
 DEBUG = True
 DATABASE = './parekori.db'
@@ -13,5 +15,12 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 if __name__ == '__main__':
+    init_db()
     app.run()
