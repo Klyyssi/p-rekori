82514fbc93435aa19e560aecc3fb77e99c480554
Sauli Ruuska <sauli.ruuska@jalonne.fi>
2016-03-23 13:01:10 +0200

    Fix schema to do nothing when reapplied

--- schema.sql
+++ schema.sql
@@ -1,15 +1,15 @@
-CREATE TABLE snippet_types_ (
+CREATE TABLE IF NOT EXISTS snippet_types_ (
     id_ integer PRIMARY KEY,
     name_ text
 );
 
-CREATE TABLE snippets_ (
+CREATE TABLE IF NOT EXISTS snippets_ (
     id_ integer PRIMARY KEY,
     type_ integer NOT NULL,
     content_ text NOT NULL
 );
 
-CREATE TABLE notes_ (
+CREATE TABLE IF NOT EXISTS notes_ (
     note_ integer,
     snippet_ integer,
     PRIMARY KEY (note_, snippet_)
