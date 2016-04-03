6260dc8cb2e898dc31c247914a3c1b33deaf2c45
Sauli Ruuska <sauli.ruuska@jalonne.fi>
2016-04-03 18:24:26 +0300

    Fix database schema to separate notes from their mapping to snippets

--- schema.sql
+++ schema.sql
@@ -5,12 +5,17 @@ CREATE TABLE IF NOT EXISTS snippet_types_ (
 
 CREATE TABLE IF NOT EXISTS snippets_ (
     id_ integer PRIMARY KEY,
-    type_ integer NOT NULL,
+    type_ integer NOT NULL, -- foreign key references snippet_types_
     content_ text NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS notes_ (
-    note_ integer,
-    snippet_ integer,
+    id_ integer PRIMARY KEY,
+    summary_ integer -- foreign key references snippets_
+);
+
+CREATE TABLE IF NOT EXISTS note_snippets_ (
+    note_ integer, -- foreign key references notes_
+    snippet_ integer, -- foreign key references snippets_
     PRIMARY KEY (note_, snippet_)
 ) WITHOUT ROWID;
