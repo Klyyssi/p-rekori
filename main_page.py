5f8e4e767050fb733c8ca5edd40e003e1021f5ec
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-04-04 18:37:29 +0300

    wraps main_page routes into a function

--- main_page.py
+++ main_page.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template
 
-main_page = Blueprint("main_page", __name__, template_folder="templates")
+def get_main_page_routes(db_execute):
+    main_page = Blueprint("main_page", __name__, template_folder="templates")
 
-@main_page.route("/")
-def index():
-    return render_template("index.html")
+    @main_page.route("/")
+    def index():
+        notes = db_execute('select * from snippets_ where type_=2')
+        return render_template("index.html", notes=notes)
+
+    return main_page
 
 
