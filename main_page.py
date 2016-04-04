a63edc2a1d05f8b2ac7654969f30d87c253005bf
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-04-04 18:51:41 +0300

    gives each row attributes id and content so it can be displayed in ui

--- main_page.py
+++ main_page.py
@@ -5,7 +5,10 @@ def get_main_page_routes(db_execute):
 
     @main_page.route("/")
     def index():
-        notes = db_execute('select * from snippets_ where type_=2')
+        notes = map(
+            lambda row: dict(id=row[0], content=row[2]),
+            db_execute('select * from snippets_ where type_=2'))
+
         return render_template("index.html", notes=notes)
 
     return main_page
